d5e91c77dce16e7cd1b5d60b62acad60b41aec0a
Adrian Mucha <kestarumper8@gmail.com>
2017-11-23 22:56:04 +0100

    working pipes

--- lsh.c
+++ lsh.c
@@ -7,12 +7,16 @@
 
 #define LSH_BUFSIZE 512
 #define LSH_TOKEN_DELIMITERS " \t\r\n"
+#define READ_FD 0
+#define WRITE_FD 1
 
-char ** lsh_split(char *);
+char ** lsh_split(char *, char *);
 
 void ctrl_c_handler(int dummy)
 {
   printf("\n");
+  printf("> ");
+  fflush(STDIN_FILENO);
 }
 
 int lsh_inbuilt_cd(char ** args) {
@@ -23,11 +27,11 @@ int lsh_inbuilt_cd(char ** args) {
       perror("lsh");
     }
   }
-  return 1;
+  return EXIT_SUCCESS;
 }
 
 int lsh_inbuilt_exit(char ** args) {
-  return EXIT_SUCCESS;
+  exit(EXIT_SUCCESS);
 }
 
 int (*lsh_inbuilt_functions[])(char **) = {
@@ -42,120 +46,91 @@ char * lsh_inbuilt_names[] = {
 
 int lsh_inbuilt_functions_size = (sizeof(lsh_inbuilt_names) / sizeof(char*));
 
-int lsh_exe(char ** args, int in, int out)
+pid_t spawn_process(char ** args, int in, int out)
 {
   pid_t pid;
-  pid_t wpid;
   int status;
-  int run_in_bg = 0;
-
-  int argc = 0;
-  for(; args[argc] != NULL; argc++) {
-    if(strcmp(args[argc], "&") == 0) {
-      args[argc] = NULL;
-      run_in_bg = 1;
-      break;
-    }
-  }
 
   pid = fork();
+  if(pid == 0) {
+  fprintf(stdout, "%s: IN[%i] --> OUT[%i]\n", args[0], in, out);
+    if(in != READ_FD) {
+      dup2(in, STDIN_FILENO);
+      close(in);
+    }
+    if(out != WRITE_FD) {
+      dup2(out, STDOUT_FILENO);
+      close(out);
+    }
 
-  if(run_in_bg) {
-    if(pid == 0) {
-      if(!fork()) {
-        // SECOND CHILD PROCESS
-        printf("+[PID: %i] %s\n", getpid(), args[0]);
-        if(execvp(args[0], args) == -1) {
-          perror("lsh:exe:execvp");
-        }
-        exit(EXIT_FAILURE);
-      }
-      // First child exits immediately
-      exit(EXIT_SUCCESS);
-    } else {
-      do {
-        wpid = waitpid(pid, &status, WUNTRACED);
-      } while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    if(execvp(args[0], args) == -1) {
+      fprintf(stderr, "lsh:spawn:execvp = %s\n", args[0]);
     }
-  } else {
-    if(pid == 0) {
-      // CHILD PROCESS
-      if (in != 0)
-        {
-          dup2 (in, 0);
-          close (in);
-        }
-      if (out != 1)
-        {
-          dup2 (out, 1);
-          close (out);
-        }
-        printf("STARTUJE PROCES %s\n", args[0]);
-      if(execvp(args[0], args) == -1) {
-        perror("lsh:exe:execvp");
-      }
-      exit(EXIT_FAILURE);
-    } else if(pid < 0) {
-      perror("lsh:exe:fork");
-      exit(EXIT_FAILURE);
-    } else {
-      do {
-        wpid = waitpid(pid, &status, WUNTRACED);
-      } while(!WIFEXITED(status) && !WIFSIGNALED(status));
+
+    exit(EXIT_FAILURE);
+  }
+
+  return pid;
+}
+
+int lsh_launch(char ** commands, int n, int run_in_bg)
+{
+  char ** com_args;
+
+  pid_t pid;
+  pid_t wpid;
+  int status = EXIT_SUCCESS;
+
+  int i = 0;
+
+  com_args = lsh_split(commands[0], LSH_TOKEN_DELIMITERS);
+
+  // Check if not empty
+  if (com_args[0] == NULL) {
+    // An empty command was entered.
+    return EXIT_SUCCESS;
+  }
+
+  int fd[2];
+  int in = READ_FD;
+  for (i = 0; i < n - 1; ++i)
+  {
+    com_args = lsh_split(commands[i], LSH_TOKEN_DELIMITERS);
+
+    if(pipe(fd)) {
+      perror("lsh:launch:pipe");
+      return 1;
     }
+
+    printf("%i zapisuje do %i\n", fd[WRITE_FD], fd[READ_FD]);
+
+    pid = spawn_process(com_args, in, fd[WRITE_FD]);
+    close(fd[WRITE_FD]);
+
+    in = fd[READ_FD];
+  }
+
+  printf("in PARENT = %i\n", in);
+
+  // Kod ktory zabral mi 8h mojego zycia
+  // dup2 byl wykonywany rowniez w funkcji spawn_process
+  // co prowadzilo do... nie wiem czego, ale chyba zamykalo pipe'a
+  // z ktorego potem szly same EOF'y
+  // if (in != READ_FD) {
+  //   dup2(in, STDIN_FILENO);
+  // }
+
+  com_args = lsh_split(commands[i], LSH_TOKEN_DELIMITERS);
+  pid = spawn_process(com_args, in, WRITE_FD);
+
+  // wait (or dont) for process
+  if(!run_in_bg) {
+    do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while(!WIFEXITED(status) && !WIFSIGNALED(status));
   }
 
   return 1;
-}
-
-int lsh_launch(char ** pipes, int n)
-{
-  char ** args;
-  int i;
-
-  args = lsh_split(pipes[0]);
-
-  if (args[0] == NULL) {
-    // An empty command was entered.
-    return 1;
-  }
-
-  for (i = 0; i < lsh_inbuilt_functions_size; i++) {
-    if (strcmp(args[0], lsh_inbuilt_names[i]) == 0) {
-      return (*lsh_inbuilt_functions[i])(args);
-    }
-  }
-
-  int in;
-  int fd [2];
-
-  /* The first process should get its input from the original file descriptor 0.  */
-  in = 0;
-
-  /* Note the loop bound, we spawn here all, but the last stage of the pipeline.  */
-  for (i = 0; i < n - 1; ++i)
-    {
-      pipe(fd);
-
-      /* f [1] is the write end of the pipe, we carry `in` from the prev iteration.  */
-      printf("Launching process %s\n", pipes[i]);
-      lsh_exe(args, in, fd[1]);
-
-      /* No need for the write end of the pipe, the child will write here.  */
-      close (fd[1]);
-
-      /* Keep the read end of the pipe, the next child will read from there.  */
-      in = fd[0];
-
-      args = lsh_split(pipes[i+1]);
-    }
-
-  /* Last stage of the pipeline - set stdin be the read end of the previous pipe
-     and output to the original file descriptor 1. */  
-  if (in != 0)
-    dup2 (in, 0);
-
-  return lsh_exe(args, in, 1);
 }
 
 char * lsh_readline()
@@ -174,9 +149,10 @@ char * lsh_readline()
     c = getchar();
 
     if(position == 0 && c == EOF) {
-      printf("\n");
+      printf("\nBye :)\n");
       exit(EXIT_SUCCESS);
-    } else if(c == '\n' || c == EOF) {
+    } else
+     if(c == '\n' || c == EOF) {
       buffer[position] = '\0';
       return buffer;
     } else {
@@ -187,7 +163,7 @@ char * lsh_readline()
   }
 }
 
-char ** lsh_split(char * line)
+char ** lsh_split(char * line, char * delims)
 {
   int bufsize = LSH_BUFSIZE;
   char * token;
@@ -199,7 +175,7 @@ char ** lsh_split(char * line)
     exit(EXIT_FAILURE);
   }
 
-  token = strtok(line, LSH_TOKEN_DELIMITERS);
+  token = strtok(line, delims);
   int pos = 0;
   while(token != NULL) {
     tokens[pos] = token;
@@ -217,7 +193,7 @@ char ** lsh_split(char * line)
       }
     }
 
-    token = strtok(NULL, LSH_TOKEN_DELIMITERS);
+    token = strtok(NULL, delims);
   }
   // add NULL at the end to signal last element
   tokens[pos] = NULL;
@@ -225,9 +201,9 @@ char ** lsh_split(char * line)
   return tokens;
 }
 
-
 // ls -l / | head | sort
-char ** lsh_pipes(char * line) {
+char ** lsh_pipes(char * line) 
+{
   int bufsize = LSH_BUFSIZE;
   char * token;
   char ** tokens = malloc(sizeof(char*) * bufsize);
@@ -267,24 +243,36 @@ char ** lsh_pipes(char * line) {
 void lsh_loop()
 {
   char * line;
-  char ** args;
+  char ** commands;
   int status;
-  int argc;
+  int comnum;
+  int run_in_bg;
 
   do {
+    run_in_bg = 0;
+
     printf("> ");
 
     line = lsh_readline();
-    args = lsh_pipes(line);
-    for(argc = 0; args[argc] != NULL; argc++);
-    status = lsh_launch(args, argc);
 
-  } while(status);
+    if(strchr(line, '&')) {
+      run_in_bg = 1;
+    }
+
+    commands = lsh_pipes(line);
+
+    for(comnum = 0; commands[comnum] != NULL; comnum++) {
+      // printf("#%i %s\n", comnum, commands[comnum]);
+    }
+
+    status = lsh_launch(commands, comnum, run_in_bg);
+
+  } while(1);
 }
 
 int main() 
 {
-  printf("LSH Shell\n");
+  printf("LSH Alpha\n");
 
   signal(SIGINT, ctrl_c_handler);
 
